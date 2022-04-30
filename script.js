93dc46aaf752bae9e0a763b33ce9a2b1d459f649
ammxr <ammarhakim57@gmail.com>
2022-04-29 21:19:24 -0400

    Updated Plotted Point and Removed Animation
    Opaque edge Points. No longer requires animation, is initialized on execution.

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
+let img;
 const data = {
 	p1: [340,100], 
   p2: [30,100],
-  p3: [100,100]
+  p3: [100,100],
+  p4: [350,100]
 };
 
-let img;
+//  figure out range based off picture Wednesday add text label for school 
+
+// For Resolution 600x400
+const xRange = [20,580]
+const yRange = [20,380]
+
 
 function preload() {
   img = loadImage('jfss.jpg');
@@ -13,17 +20,21 @@ function setup() {
   createCanvas(620, 620);
   background(50);
   image(img, 10, 10, 600, 600);
-  frameRate(1);
-}
-
-function draw() {
   for (let value in data) {
     const dictValue = data[value]
     const x = dictValue[0];
     const y =  dictValue[1];
-    fill(255,0,0);
+    fill(255,0,0,155);
+    strokeWeight(5);
+    stroke(255, 150, 200,90);
     circle(x, y, 10);
     console.log("Printed circle at ("+ x +","+y+")");
   }
-  console.log("-")
+  console.log("\n- - - - Task Complete - - - - ")
+}
+
+
+
+
+function draw() {
 }

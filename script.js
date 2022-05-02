ad677b038658e153975224a424ec0a323444008e
ammxr <ammarhakim57@gmail.com>
2022-05-02 09:06:23 -0400

    Plot Range/Divisions
    Divisions of data points. Documentation + Canvas Update

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
+// Notes: 2k x 2k export as jpeg
+
 let img;
+
 const data = {
 	p1: [340,100], 
   p2: [30,100],
@@ -6,21 +9,49 @@ const data = {
   p4: [350,100]
 };
 
-const xRange = [20,580]
-const yRange = [20,380]
+const pasteRange = {
+  stress: [[20,100],[20,100]],
+  depression: [[10,100],[200,400]]
+};
 
-
-function preload() {
-  img = loadImage('jfss.jpg');
-}
 function setup() {
-  createCanvas(620, 620);
-  background(50);
-  image(img, 10, 10, 600, 600);
+  createCanvas(2000, 2000);
+  rect(10, 10, 1980, 1980, 20, 20, 20, 20);
+
+  
+
+  /* Category Assignment */
   for (let value in data) {
     const dictValue = data[value]
-    const x = dictValue[0];
-    const y =  dictValue[1];
+    const pasteRangeXStress = pasteRange['stress']
+    const pasteRangeYStress = pasteRange['stress']
+    
+    /* Range of possible X-values for data point */
+    let rangeX = {
+      min: pasteRangeXStress[0][0], 
+      max: pasteRangeXStress[0][1]
+    }
+    
+    /* Random X-coordinate within range */
+    let deltaX = rangeX.max - rangeX.min 
+    const stressRandX = Math.round(rangeX.min + Math.random() * deltaX)
+
+    /* Range of possible Y-values for data point */
+    let rangeY = {
+      min: pasteRangeYStress[1][0], 
+      max: pasteRangeYStress[1][1]
+    }
+
+    /* Random Y-coordinate within range */
+    let deltaY = rangeY.max - rangeY.min 
+    const stressRandY = Math.round(rangeY.min + Math.random() * deltaY)
+
+    /* Point Coordinate */
+    let x = stressRandX;
+    let y =  stressRandY;
+
+    /* Point */
+    blendMode(MULTIPLY);
     fill(255,0,0,155);
     strokeWeight(5);
     stroke(255, 150, 200,90);
